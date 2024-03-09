b990a3b455d4a47c4202911f7c4147610db66e55
Rishav Sharma <rishishah907@gmail.com>
2024-03-09 15:36:41 +0530

    testing

--- backend/TEMP_BUILD.sh
+++ backend/TEMP_BUILD.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r backend/requirements.txt
+pip install -r requirements.txt
 
 # Changing directory
 cd ../frontend/
